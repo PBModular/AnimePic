69a3be9e2038384d03601f502843db252d277a76
Ultra119 <pasasvistak0@gmail.com>
2023-06-19 23:40:37 +0300

    main: Handle errors

--- main.py
+++ main.py
@@ -28,26 +28,31 @@ class AnimeModule(BaseModule):
         async with AsyncGelbooru(api_key=self.api_key, user_id=self.user_id) as gel:
             try:
                 results = await gel.search_posts(tags, limit=int(limit), random=True)
-                self.logger.info(tags)
-                self.logger.info(limit)
             except KeyError:
                 await message.reply_text(self.S["tags_not_found"])
                 return
             
-            if results:
-                for i in results:
-                    file_url = str(i.file_url)
-                    chat_id = message.chat.id
-                    if chat_id not in self.sent_photos or file_url not in self.sent_photos[chat_id]:
-                        try: 
-                            await message.reply_photo(
-                                photo=file_url,
-                                caption=self.S['credit'].format(file_url=file_url)
-                            )
-                            
-                            self.sent_photos.setdefault(chat_id, []).append(file_url)
-                            await asyncio.sleep(1)
-                        except errors.WebpageCurlFailed as e:
-                            self.logger.error(e)
-        
+        if results:
+            for photo in results:
+                file_url = str(photo.file_url)
+                chat_id = message.chat.id
+                if chat_id not in self.sent_photos or file_url not in self.sent_photos[chat_id]:
+                    try: 
+                        await message.reply_photo(
+                            photo=file_url,
+                            caption=self.S['credit'].format(file_url=file_url)
+                        )
+                        
+                        self.sent_photos.setdefault(chat_id, []).append(file_url)
+                        await asyncio.sleep(1)
+                        
+                    except errors.WebpageCurlFailed as e:
+                        self.logger.warning(e)
+                        continue
+                
+                    except errors.FloodWait as e:
+                        self.logger.warning(e)
+                        await asyncio.sleep(31)
+                        continue
+                        
         asyncio.ensure_future(self.clear_sent_photos(message.chat.id))
